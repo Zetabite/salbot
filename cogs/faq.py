9144dc09692435997daa475d90bd31fe90839cb3
laundmo <laurinschmidt2001@gmail.com>
2020-01-28 08:34:32 +0100

    changed regex and mesage

--- cogs/faq.py
+++ cogs/faq.py
@@ -2,6 +2,7 @@ import discord
 import json
 import inspect
 import re
+import typing
 from discord.ext import commands
 
 def FAQMessage_factory(bot, names, regexes, channel_whitelist, message):
@@ -27,10 +28,13 @@ def FAQMessage_factory(bot, names, regexes, channel_whitelist, message):
             if message.author.id != self.bot.user.id: # check for the bot beign the author
                 if message.channel.id in self.channel_whitelist:# check for channel in channel_whitelist
                     if any(r.search(message.content) for r in self.regexes): # check if any of the regexes are matched
-                        await self.send(message.channel) # send the message using the method below
+                        await self.send(message.channel, message.author) # send the message using the method below
 
-        async def send(self, channel): # this is a seperate method because of the command
-            await channel.send(self.message) # simply send the message
+        async def send(self, channel, member: typing.Optional[discord.Member] = None): # this is a seperate method because of the command
+            message = ""
+            if member:
+                message += member.mention + "\n"
+            await channel.send(message + self.message) # simply send the message
 
     return FAQMessage(bot, names, regexes, channel_whitelist, message)
 
@@ -38,8 +42,8 @@ def setup(bot):
     channels = [666575359411748875, 666758275504537604, 666813360867770388, 660701994549379125, 669119687530905613]
     
     faq_messages = [
-        FAQMessage_factory(bot, ["dream", "dreams"], [r"dream.?s?.?method",], channels, ">>> Hey, it looks like you mentioned Dream! Unfortunately we cannot use that method because we don't have enough information. But here's some other ideas: {link to a google doc}"),
-        FAQMessage_factory(bot, ["seed",], [r"seed.is", r"have.?the.?seed"], channels, ">>> Hey, it looks like you mentioned what the seed is! If you actually found the seed, please message a mod. If you're saying this as a joke, please dont :)"),
+        FAQMessage_factory(bot, ["dream", "dreams"], [r"dream.?s?.?method",], channels, ">>> Hey, it looks like you mentioned Dream! Unfortunately we cannot use that method because we don't have enough information."),
+        FAQMessage_factory(bot, ["seed",], [r"seed.is(?!n)", r"have.?the.?seed"], channels, ">>> Hey, it looks like you mentioned what the seed is! If you actually found the seed, please message a mod. If you're saying this as a joke, please dont :)"),
         FAQMessage_factory(bot, ["supercomputer", "supercomp", "sc"], [r"super.?comput."], channels, ">>> Hey, it looks like you mentioned a supercomputer! Thankfully `@cactus uwu#0523` is dedicating several supercomputers towards this!"),
         FAQMessage_factory(bot, ["quantumcomputer", "qcomp", "qc"], [r"quantum.?comput."], channels, ">>> Hey, it looks like you mentioned a quantum computer! Unfortunately this won't help with this problem and we already have enough computing power"),
         FAQMessage_factory(bot, ["ihelp",], [r"(?:(?:can|may).?i.?help)", r"(do.?to.?help)"], channels, ">>> Hey, it looks like want to help! If you have reverse-engineering or coding experience head to <#666758275504537604>, otherwise you can help a recreation project in <#666813360867770388>"),
