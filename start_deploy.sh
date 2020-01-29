df9ec3edf3a4d40909953fbff5e42d4e92f8fb25
Rain <54524082+rain-py@users.noreply.github.com>
2020-01-29 03:02:15 -0800

    removed sudo

--- start_deploy.sh
+++ start_deploy.sh
@@ -1,9 +1,9 @@
 exitcode=69
 while [ exitcode -eq 69 ]
 do
-    sudo git stash push
-    sudo git fetch --all
-    sudo git reset --hard origin/deploy
+    git stash push
+    git fetch --all
+    git reset --hard origin/deploy
     python3 bot2.py sc
     exitcode=${?}
 done
