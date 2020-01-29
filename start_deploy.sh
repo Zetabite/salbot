90e0c8736d2f504341bff3cbf0d0911b027e44a1
rain-py <nelmer@student.sierracollege.edu>
2020-01-29 11:28:36 +0000

    working deploy script :D

--- start_deploy.sh
+++ start_deploy.sh
@@ -1,9 +1,5 @@
-exitcode=69
-while [ exitcode == 69 ];
-do
+until python3 bot2.py sc;do   
     git stash push
     git fetch --all
     git reset --hard origin/deploy
-    python3 bot2.py sc
-    exitcode=${?}
 done
