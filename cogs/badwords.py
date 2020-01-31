460e65eb98d0b6c9b2e6e5e648cabca60bcfb14b
Rain <54524082+rain-py@users.noreply.github.com>
2020-01-31 00:45:39 -0800

    removed bot.event

--- cogs/badwords.py
+++ cogs/badwords.py
@@ -5,8 +5,7 @@ import typing
   
 bad_words = ["nigger", "faggot", "pornhub.com", "discord.gg", "test123"]
 
-@bot.event
-async def on_message(message):
+def on_message(message):
     message_content = message.content.strip().lower()
     for bad_word in bad_words:
         if bad_word in message.content:
