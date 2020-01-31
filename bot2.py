2c4b1f5622a44f5becb4a82bb9f872d98a03b9c3
Rain <54524082+rain-py@users.noreply.github.com>
2020-01-31 00:37:48 -0800

    addded cogs.badwords

--- bot2.py
+++ bot2.py
@@ -23,6 +23,7 @@ except IndexError:
 async def on_ready():
     client.load_extension("cogs.user_info")
     client.load_extension("cogs.faq")
+    client.load_extension("cogs.badwords")
     await client.change_presence(status=discord.Status.online, activity=discord.CustomActivity('Leaking salc\'s base in progress'))
     print(f'{client.user} has connected to Discord!')
 
