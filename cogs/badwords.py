d78cf46df7241a332c1b1b390c398c37d8b3a0cb
Rain <54524082+rain-py@users.noreply.github.com>
2020-02-07 19:41:02 -0800

    Update badwords.py

--- cogs/badwords.py
+++ cogs/badwords.py
@@ -19,7 +19,7 @@ class Badwords(commands.Cog):
             # Remove the message which triggered the bot
             await message.delete()
             # Send reply/notification
-            await message.channel.send(f':eyes: {message.author}')
+            await message.channel.send(f':eyes')
             automation_logger.info(f"user {message.author} ({message.author.id}) sent bad word in channel {message.channel.name}, message: \"{message.content[0:1500]}\" ")
 
 
