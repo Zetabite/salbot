b680bf52db045a335f8ba7e0d2b2b1bf33da6901
laundmo <laurinschmidt2001@gmail.com>
2020-05-04 18:09:01 +0200

    delete database entries for users that say badwords

--- cogs/badwords.py
+++ cogs/badwords.py
@@ -1,14 +1,24 @@
 from discord.ext import commands
 import logging
 import discord
+import aiosqlite
+from pathlib import Path
 logger = logging.getLogger('salc1bot')
 automation_logger = logging.getLogger('salc1bot.automated')
-
+db_p = Path("./data/autorankup.db").resolve()
 
 class Badwords(commands.Cog):
     def __init__(self, bot, badwords):
         self.bot = bot
         self.badwords = badwords
+        self.bot.loop.create_task(self.ainit())
+    
+    async def ainit(self):
+        self.conn = await aiosqlite.connect(db_p)
+    
+    async def deluser(self, id):
+        await self.conn.execute(f"DELETE FROM messagecount WHERE user_id = {id};")
+        logger.debug(f"Deleted database entry for {id}")
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -19,6 +29,7 @@ class Badwords(commands.Cog):
             # Remove the message which triggered the bot
             await message.delete()
             await message.author.send("There are some words discord doesn't like, we have to filter them out.")
+            await self.deluser(message.author.id)
             automation_logger.info(f"user {message.author} ({message.author.id}) sent bad word in channel {message.channel.name}, message: \"{message.content[0:1500]}\" ")
 
 
