a21ebc662a2f74f56594d00522c36370a5f8dfd0
vcokltfre <vcokltfre@gmail.com>
2020-05-18 05:49:54 +0100

    fixed... again

--- cogs/faqv2.py
+++ cogs/faqv2.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('salc1bot')
 automation_logger = logging.getLogger('salc1bot.automated')
 
 g_channels = {
-    "channels":[ 660701994549379125, 669119687530905613, 436411303351943188, 548308507636662283],
+    "channels":[710226813808279615],#[ 660701994549379125, 669119687530905613, 436411303351943188, 548308507636662283],
     "packchannels":[666575359411748875, 666758275504537604]
 }
 
@@ -41,7 +41,7 @@ class Faq(commands.Cog):
         for item in self.json_data:
             command = FaqMessage(item["names"][0], item["content"])
             for listitem in item["regexs"]:
-                self.regexs.append(RegExMessagePair(re.compile(listitem), item["content"], item["channels"]))
+                self.regexs.append(RegExMessagePair(listitem, item["content"], item["channels"]))
             print(command.name, command.content)
             self.faq.command(item["names"][0], aliases=item["names"][1:])(command.__call__)
 
@@ -62,8 +62,8 @@ class Faq(commands.Cog):
             return
         content = ctx.content
         for item in self.regexs:
-            if item.reg.match(content) and (ctx.channel.id in item.channels) and len(ctx.author.roles) <= 1:
-                automation_logger.info(f"FAQ {self.names[0]} triggered by user {ctx.author} ({ctx.author.id}) in {ctx.channel.name}")
+            print(re.search(item.reg, content))
+            if re.search(item.reg, content) and (ctx.channel.id in item.channels) and len(ctx.author.roles) <= 1:
                 await ctx.channel.send(item.content)
                 return
 
