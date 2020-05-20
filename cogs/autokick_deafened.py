2163489c6b13a1b974b01f8c9861f15721f2928e
laundmo <laurinschmidt2001@gmail.com>
2020-05-20 20:54:10 +0200

    better anti deafen

--- cogs/autokick_deafened.py
+++ cogs/autokick_deafened.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import logging
+import asyncio
 automation_logger = logging.getLogger('salc1bot.automated')
 
 class Antideafen(commands.Cog):
@@ -9,9 +10,15 @@ class Antideafen(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, state_before, state_after):
-        if state_after.self_deaf and state_before.channel != None:
+        if state_after.channel is None:
+            return
+        await asyncio.sleep(30)
+        state_after = member.voice
+        if state_after.self_deaf:
             await member.move_to(None, reason="Anti Deafen")
             automation_logger.info(f"Deafen AutoKick triggered by user {member} ({member.id})")
 
 def setup(bot):
     bot.add_cog(Antideafen(bot))
+    
+    # ^(.*)\] cactus uwu#0523
