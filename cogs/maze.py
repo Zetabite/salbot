1bdda37b1692b594aa28b2a0eea564b68029a7f6
vcokltfre <vcokltfre@gmail.com>
2020-05-30 04:53:18 +0100

    ctx.channel.send > ctx.send

--- cogs/maze.py
+++ cogs/maze.py
@@ -12,7 +12,7 @@ class Maze(commands.Cog):
 	async def on_message(self, ctx):
 		if ctx.author.id in self.mid and self.enabled:
 			if random.randint(0,100) == 5:
-				await ctx.channel.send(f"Fuck you {ctx.author.mention} >:)")
+				await ctx.send(f"Fuck you {ctx.author.mention} >:)")
 
 	@commands.has_any_role("Moderator", "Administrator", "Private Chat Access")
 	@commands.command(name='togglemaze')
