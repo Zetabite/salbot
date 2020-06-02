0efd301db345ce469bde4500004ca836393afcc5
vcokltfre <vcokltfre@gmail.com>
2020-06-02 20:18:41 +0100

    added exemptions to autoyeet

--- cogs/automaticus_yeetus.py
+++ cogs/automaticus_yeetus.py
@@ -7,11 +7,12 @@ automation_logger = logging.getLogger('salc1bot.automated')
 class Antideafen(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.exempt = [264898221665419264, 297045071457681409]
 
     #Remove people from vc when deafened f0r 30s
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, state_before, state_after):
-        if state_after.channel is None:
+        if state_after.channel is None or member.id in self.exempt:
             return
         await asyncio.sleep(30)
         state_after = member.voice
