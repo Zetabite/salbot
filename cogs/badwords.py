6d9488a5ca8f240afa58accd0efc052e5014055a
laundmo <laurinschmidt2001@gmail.com>
2020-06-14 01:47:13 +0200

    anit-apng

--- cogs/badwords.py
+++ cogs/badwords.py
@@ -4,9 +4,22 @@ import discord
 import aiosqlite
 import re
 import json
+import io
 
 logger = logging.getLogger('salc1bot')
 automation_logger = logging.getLogger('salc1bot.automated')
+
+async def contains_apng(message: discord.Message):
+    for a in message.attachments:
+        f = io.BytesIO()
+        await a.save(f)
+        a = f.read()
+        acTL = a.find(b"\x61\x63\x54\x4C")
+        if acTL > 0:
+            IDAT = a.find(b"\x49\x44\x41\x54")
+            if acTL < IDAT:
+                return True
+    return False
 
 class Badwords(commands.Cog):
     def __init__(self, bot, badwords):
@@ -35,6 +48,10 @@ class Badwords(commands.Cog):
             await self.deluser(message.author.id)
             automation_logger.info(f"user {message.author} ({message.author.id}) sent bad word in channel {message.channel.name}, message: \"{message.content[0:1500]}\" ")
 
+        has_apng = await contains_apng(message) 
+        if has_apng:
+            await message.delete()
+            await message.channel.send(f"> We do not allow the APNG format due to it being abused. {message.author.mention}")
 
 def setup(bot):
     with open("data/badwords.json") as f:
