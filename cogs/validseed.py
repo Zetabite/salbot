715141b780f7f63a3f295062a119cce88a7f40a4
Cactus <cactusduper1@gmail.com>
2020-06-18 09:24:28 -0500

    Small fix

--- cogs/validseed.py
+++ cogs/validseed.py
@@ -19,7 +19,7 @@ class ValidSeed(commands.Cog):
         seed = 7847617 * a - 18218081 * b
         if seed > 0:
             if(((seed * 25214903917 + 11) & ((1 << 48) - 1))) > 0:
-                nextLong = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))
+                nextLong = ((struct.unpack("@q",struct.pack("@Q",(seed >> 16 << 32)))[0]) + (((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16)%-2147483648)
             else:
                 nextLong = ((seed >> 16 << 32) + ((((seed * 25214903917 + 11) & ((1 << 48) - 1)) >> 16))+(2 <<15))
         else:
